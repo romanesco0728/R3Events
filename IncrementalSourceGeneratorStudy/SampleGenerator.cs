6b9eeb2c45c04b58bd52b7bde2fdee17791c0745
romanesco0728 <romanesco0728@gmail.com>
2026-02-03 14:17:45 +0900

    add static attribute definition

--- IncrementalSourceGeneratorStudy/SampleGenerator.cs
+++ IncrementalSourceGeneratorStudy/SampleGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
 
 namespace IncrementalSourceGeneratorStudy;
 
@@ -7,6 +9,23 @@ public partial class SampleGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
+        context.RegisterPostInitializationOutput(static ctx =>
+        {
+            var code = @"namespace Events.R3
+{
+    using System;
 
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+    internal sealed class R3EventAttribute : Attribute
+    {
+        public R3EventAttribute() { }
+
+        public string? Name { get; set; }
+    }
+}"
+            ;
+
+            ctx.AddSource("Events.R3.R3EventAttribute.g.cs", SourceText.From(code, Encoding.UTF8));
+        });
     }
 }
