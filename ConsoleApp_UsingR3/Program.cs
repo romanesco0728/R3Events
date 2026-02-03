57b39ecbaf272461a6d208e655de338e9e2cc874
romanesco0728 <romanesco0728@gmail.com>
2026-02-03 13:39:38 +0900

    add proto

---
+++ ConsoleApp_UsingR3/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using R3;
+using System.ComponentModel;
+
+Console.WriteLine("Hello, World!");
+
+class C1
+{
+    public event EventHandler? MyEvent1;
+    public event CancelEventHandler? MyEvent2;
+}
+
+static class C1Extensions
+{
+    public static Observable<Unit> MyEvent1AsObservable(this C1 c1, CancellationToken cancellationToken = default)
+    {
+        var rawObservable = Observable.FromEventHandler(
+            h => c1.MyEvent1 += h,
+            h => c1.MyEvent1 -= h,
+            cancellationToken
+            );
+        return rawObservable.AsUnitObservable();
+    }
+    public static Observable<CancelEventArgs> MyEvent2AsObservable(this C1 c1, CancellationToken cancellationToken = default)
+    {
+        var rawObservable = Observable.FromEvent<CancelEventHandler, (object?, CancelEventArgs Args)>(
+            static h => new CancelEventHandler((s, e) => h((s, e))),
+            h => c1.MyEvent2 += h,
+            h => c1.MyEvent2 -= h,
+            cancellationToken
+            );
+        return rawObservable.Select(ep => ep.Args);
+    }
+}
