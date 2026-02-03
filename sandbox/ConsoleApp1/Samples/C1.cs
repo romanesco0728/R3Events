0f85a222b6f9158caa280709ab98b6dfed5cf215
romanesco0728 <romanesco0728@gmail.com>
2026-02-03 17:48:49 +0900

    add CancelEventHandler event to test model

--- sandbox/ConsoleApp1/Samples/C1.cs
+++ sandbox/ConsoleApp1/Samples/C1.cs
@@ -1,6 +1,9 @@
-﻿namespace ConsoleApp1.Samples;
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1.Samples;
 
 internal class C1
 {
     public event EventHandler? MyEvent1;
+    public event CancelEventHandler? MyEvent2;
 }
