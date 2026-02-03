8b1719141ba026c1e1fbd970602fa71359bca54b
romanesco0728 <romanesco0728@gmail.com>
2026-02-03 14:49:24 +0900

    refactor

--- IncrementalSourceGeneratorStudy/SampleGenerator.cs
+++ IncrementalSourceGeneratorStudy/SampleGenerator.cs
@@ -9,33 +9,32 @@ public partial class SampleGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        context.RegisterPostInitializationOutput(static ctx =>
-        {
-            var code = @"namespace Events.R3
-{
-    using global::System;
-
-    [global::System.AttributeUsage(global::System.AttributeTargets.Class | global::System.AttributeTargets.Struct | global::System.AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
-    internal sealed class R3EventAttribute : global::System.Attribute
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref=""global::Events.R3.R3EventAttribute""/> class with the specified target type.
-        /// </summary>
-        /// <param name=""type"">The target <see cref=""global::System.Type""/> the attribute refers to.</param>
-        public R3EventAttribute(global::System.Type type)
-        {
-            this.Type = type ?? throw new global::System.ArgumentNullException(nameof(type));
-        }
-
-        /// <summary>
-        /// Gets the target <see cref=""global::System.Type""/> represented by this attribute.
-        /// </summary>
-        public global::System.Type Type { get; }
+        context.RegisterPostInitializationOutput(EmitDefaultAttribute);
     }
-}"
-            ;
 
-            ctx.AddSource("Events.R3.R3EventAttribute.g.cs", SourceText.From(code, Encoding.UTF8));
-        });
+    private static void EmitDefaultAttribute(IncrementalGeneratorPostInitializationContext context)
+    {
+        var code = """
+            namespace Events.R3
+            {
+                [global::System.AttributeUsage(global::System.AttributeTargets.Class | global::System.AttributeTargets.Struct | global::System.AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+                internal sealed class R3EventAttribute : global::System.Attribute
+                {
+                    /// <summary>
+                    /// Initializes a new instance of the <see cref="global::Events.R3.R3EventAttribute"/> class with the specified target type.
+                    /// </summary>
+                    /// <param name="type">The target <see cref="global::System.Type"/> the attribute refers to.</param>
+                    public R3EventAttribute(global::System.Type type)
+                    {
+                        this.Type = type ?? throw new global::System.ArgumentNullException(nameof(type));
+                    }
+                    /// <summary>
+                    /// Gets the target <see cref="global::System.Type"/> represented by this attribute.
+                    /// </summary>
+                    public global::System.Type Type { get; }
+                }
+            }
+            """;
+        context.AddSource("Events.R3.R3EventAttribute.g.cs", SourceText.From(code, Encoding.UTF8));
     }
 }
