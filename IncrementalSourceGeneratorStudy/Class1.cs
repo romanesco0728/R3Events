98d32b062bc55caac29ba9a91548dc579d7f4f0d
romanesco0728 <romanesco0728@gmail.com>
2026-02-03 12:40:10 +0900

    construct debug environment

--- IncrementalSourceGeneratorStudy/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace IncrementalSourceGeneratorStudy
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ IncrementalSourceGeneratorStudy/SampleGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace IncrementalSourceGeneratorStudy;
+
+[Generator(LanguageNames.CSharp)]
+public partial class SampleGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+
+    }
+}
