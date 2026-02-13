82edf423efd85abf99ea77c7f97d33ea1b057e8f
romanesco0728 <romanesco0728@gmail.com>
2026-02-13 17:35:08 +0900

    add age and its test

--- tests/EventsR3Generator.Tests/Models/Person.cs
+++ tests/EventsR3Generator.Tests/Models/Person.cs
@@ -1,9 +1,13 @@
 ﻿namespace EventsR3Generator.Tests.Models;
 
-internal sealed class Person(string name)
+internal sealed class Person(string name, int age)
 {
     public event EventHandler? NameChanged;
+    public event EventHandler<int>? AgeChanged;
+
     private string? _name = name;
+    private int _age = age;
+
     public string? Name
     {
         get => _name;
@@ -13,6 +17,18 @@ internal sealed class Person(string name)
             {
                 NameChanged?.Invoke(this, EventArgs.Empty);
                 _name = value;
+            }
+        }
+    }
+    public int Age
+    {
+        get => _age;
+        set
+        {
+            if (_age != value)
+            {
+                AgeChanged?.Invoke(this, value);
+                _age = value;
             }
         }
     }
