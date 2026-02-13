2768420907b9b8aaa8b0663d15d610152b2cfc7c
romanesco0728 <romanesco0728@gmail.com>
2026-02-13 15:15:08 +0900

    wip

---
+++ tests/EventsR3Generator.Tests/Extensions/PersonExtensions.cs
@@ -0,0 +1,6 @@
+﻿using Events.R3;
+
+namespace EventsR3Generator.Tests.Extensions;
+
+[R3Event(typeof(Models.Person))]
+internal static partial class PersonExtensions;
---
+++ tests/EventsR3Generator.Tests/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace EventsR3Generator.Tests.Models;
+
+internal sealed class Person(string name)
+{
+    public event EventHandler? NameChanged;
+    private string? _name = name;
+    public string? Name
+    {
+        get => _name;
+        set
+        {
+            if (_name != value)
+            {
+                NameChanged?.Invoke(this, EventArgs.Empty);
+                _name = value;
+            }
+        }
+    }
+}
