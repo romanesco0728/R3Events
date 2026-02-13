68da46a02f1d16d9f67f4cdcbfa6f0f175739e89
romanesco0728 <romanesco0728@gmail.com>
2026-02-13 16:47:36 +0900

    add an error test

---
+++ tests/EventsR3Generator.Tests/ErrorTests.cs
@@ -0,0 +1,27 @@
+﻿using EventsR3Generator.Tests.Utilities;
+
+namespace EventsR3Generator.Tests;
+
+[TestClass]
+public sealed class ErrorTests
+{
+    [TestMethod]
+    public void R3EventOnNonStaticClass_ShouldProduceER003Error()
+    {
+        // lang=C#-test
+        var source = """
+namespace ErrorTest;
+
+[Events.R3.R3Event(typeof(int))]
+public partial class IntExtensions
+{
+}
+""";
+
+        var result = CSharpGeneratorRunner.RunGenerator(source);
+
+        Assert.IsNotNull(result);
+        Assert.HasCount(1, result);
+        Assert.AreEqual("ER003", result[0].Id);
+    }
+}
