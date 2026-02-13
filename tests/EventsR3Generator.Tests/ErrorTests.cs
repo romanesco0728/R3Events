aaa6b6cfb229b5c4107f8b141214502adf06f7fc
romanesco0728 <romanesco0728@gmail.com>
2026-02-13 17:09:41 +0900

    add error tests

--- tests/EventsR3Generator.Tests/ErrorTests.cs
+++ tests/EventsR3Generator.Tests/ErrorTests.cs
@@ -7,6 +7,47 @@ namespace EventsR3Generator.Tests;
 public sealed class ErrorTests
 {
     [TestMethod]
+    public void R3EventOnNonPartialClass_ShouldProduceER001Error()
+    {
+        // lang=C#-test
+        var source = """
+namespace ErrorTest;
+
+[Events.R3.R3Event(typeof(int))]
+public static class IntExtensions
+{
+}
+""";
+
+        var result = CSharpGeneratorRunner.RunGenerator(source);
+
+        result.Length.ShouldBe(1, "Generator should produce exactly one diagnostic");
+        result[0].Id.ShouldBe("ER001", "Diagnostic ID should be ER001 for non-partial class error");
+    }
+
+    [TestMethod]
+    public void R3EventOnNestedClass_ShouldProduceER002Error()
+    {
+        // lang=C#-test
+        var source = """
+namespace ErrorTest;
+
+public static class OuterClass
+{
+    [Events.R3.R3Event(typeof(int))]
+    public static partial class IntExtensions
+    {
+    }
+}
+""";
+
+        var result = CSharpGeneratorRunner.RunGenerator(source);
+
+        result.Length.ShouldBe(1, "Generator should produce exactly one diagnostic");
+        result[0].Id.ShouldBe("ER002", "Diagnostic ID should be ER002 for nested class error");
+    }
+
+    [TestMethod]
     public void R3EventOnNonStaticClass_ShouldProduceER003Error()
     {
         // lang=C#-test
@@ -23,5 +64,24 @@ public partial class IntExtensions
 
         result.Length.ShouldBe(1, "Generator should produce exactly one diagnostic");
         result[0].Id.ShouldBe("ER003", "Diagnostic ID should be ER003 for non-static class error");
+    }
+
+    [TestMethod]
+    public void R3EventOnGenericClass_ShouldProduceER004Error()
+    {
+        // lang=C#-test
+        var source = """
+namespace ErrorTest;
+
+[Events.R3.R3Event(typeof(int))]
+public static partial class IntExtensions<T>
+{
+}
+""";
+
+        var result = CSharpGeneratorRunner.RunGenerator(source);
+
+        result.Length.ShouldBe(1, "Generator should produce exactly one diagnostic");
+        result[0].Id.ShouldBe("ER004", "Diagnostic ID should be ER004 for generic class error");
     }
 }
