807f040d772ed2479a8b8fc30ed854dda66cf0d2
romanesco0728 <romanesco0728@gmail.com>
2026-02-25 17:19:30 +0900

    refactor: enhance CodeRegenerationTests with additional scenarios for event tracking

--- src/tests/R3EventsGenerator.Tests/CodeRegenerationTests.cs
+++ src/tests/R3EventsGenerator.Tests/CodeRegenerationTests.cs
@@ -42,7 +42,110 @@ public static partial class PersonExtensions
 
         var reasons = CSharpGeneratorRunner.GetIncrementalGeneratorTrackedStepsReasons("R3Events.", step1, step2);
 
-        reasons[0][0].Reasons.ShouldBe("New");
-        reasons[1][0].Reasons.ShouldBe("Modified");
+        reasons[0][0].Reasons.ShouldBe("New", "First run should be tracked as New for event-added scenario");
+        reasons[1][0].Reasons.ShouldBe("Modified", "Second run should be tracked as Modified when an event is added to the target class");
+    }
+
+    [TestMethod]
+    public void TargetClassEventRemoved_ShouldBeTrackedAsModified()
+    {
+        // lang=C#-test
+        var step1 = """
+namespace IncrementalTest;
+
+public class Person
+{
+    public event System.EventHandler<string>? NameChanged;
+    public event System.EventHandler<int>? AgeChanged;
+}
+
+[R3Events.R3Event<Person>]
+public static partial class PersonExtensions
+{
+}
+""";
+
+        // lang=C#-test
+        var step2 = """
+namespace IncrementalTest;
+
+public class Person
+{
+    public event System.EventHandler<string>? NameChanged;
+}
+
+[R3Events.R3Event<Person>]
+public static partial class PersonExtensions
+{
+}
+""";
+
+        var reasons = CSharpGeneratorRunner.GetIncrementalGeneratorTrackedStepsReasons("R3Events.", step1, step2);
+
+        reasons[0][0].Reasons.ShouldBe("New", "First run should be tracked as New for event-removed scenario");
+        reasons[1][0].Reasons.ShouldBe("Modified", "Second run should be tracked as Modified when an event is removed from the target class");
+    }
+
+    [TestMethod]
+    public void TargetClassEventPayloadTypeChanged_ShouldBeTrackedAsModified()
+    {
+        // lang=C#-test
+        var step1 = """
+namespace IncrementalTest;
+
+public class Person
+{
+    public event System.EventHandler<string>? NameChanged;
+}
+
+[R3Events.R3Event<Person>]
+public static partial class PersonExtensions
+{
+}
+""";
+
+        // lang=C#-test
+        var step2 = """
+namespace IncrementalTest;
+
+public class Person
+{
+    public event System.EventHandler<int>? NameChanged;
+}
+
+[R3Events.R3Event<Person>]
+public static partial class PersonExtensions
+{
+}
+""";
+
+        var reasons = CSharpGeneratorRunner.GetIncrementalGeneratorTrackedStepsReasons("R3Events.", step1, step2);
+
+        reasons[0][0].Reasons.ShouldBe("New", "First run should be tracked as New for event payload-type change scenario");
+        reasons[1][0].Reasons.ShouldBe("Modified", "Second run should be tracked as Modified when an event payload type changes on the target class");
+    }
+
+    [TestMethod]
+    public void InputUnchanged_ShouldBeTrackedAsUnchanged()
+    {
+        // lang=C#-test
+        var step = """
+namespace IncrementalTest;
+
+public class Person
+{
+    public event System.EventHandler<string>? NameChanged;
+}
+
+[R3Events.R3Event<Person>]
+public static partial class PersonExtensions
+{
+}
+""";
+
+        var reasons = CSharpGeneratorRunner.GetIncrementalGeneratorTrackedStepsReasons("R3Events.", step, step);
+
+        reasons[0][0].Reasons.ShouldBe("New", "First run should be tracked as New for unchanged-input scenario");
+        reasons[1][0].Reasons.ShouldBe("Unchanged", "Second run should be tracked as Unchanged when the input source is identical");
     }
 }
